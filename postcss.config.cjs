92cf478495e7d698a436c2723aecf9beb94eeaa5
asiyashaikh02 <shkasiya02@gmail.com>
2026-04-04 17:11:59 +0530

    comit after env

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,12 @@
 module.exports = {
   plugins: {
+    // Use the new PostCSS plugin bridge for Tailwind
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  }
+}
+module.exports = {
+  plugins: {
     tailwindcss: {},
     autoprefixer: {},
   }
